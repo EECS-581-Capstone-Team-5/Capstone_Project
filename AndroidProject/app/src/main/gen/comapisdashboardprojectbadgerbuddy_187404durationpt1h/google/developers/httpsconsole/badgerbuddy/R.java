08a001668ed9f200cb60a419463f2e7a2e2ac3c3
Alex Pechin <a800p737@ku.edu>
2018-01-22 14:29:56 -0600

    Alex Test

---
+++ AndroidProject/app/src/main/gen/comapisdashboardprojectbadgerbuddy_187404durationpt1h/google/developers/httpsconsole/badgerbuddy/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package comapisdashboardprojectbadgerbuddy_187404durationpt1h.google.developers.httpsconsole.badgerbuddy;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
