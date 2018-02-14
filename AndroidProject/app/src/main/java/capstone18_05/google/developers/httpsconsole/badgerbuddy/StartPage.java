f19ba25340d8d0151b8a965641deeda2d7920718
James Muoghalu <jmuoghalu00@outlook.com>
2018-02-14 10:51:24 -0600

    Minor Update
    Working on Login Page

---
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/StartPage.java
@@ -0,0 +1,16 @@
+package capstone18_05.google.developers.httpsconsole.badgerbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+
+
+}
