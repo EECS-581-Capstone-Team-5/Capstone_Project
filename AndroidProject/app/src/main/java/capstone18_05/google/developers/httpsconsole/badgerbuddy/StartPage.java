27467b32f2e7068ae5601db58355b7347c90462a
James Muoghalu <j286m692@ku.edu>
2018-02-19 13:57:14 -0600

    Created Account Pages

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/StartPage.java
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/StartPage.java
@@ -1,7 +1,9 @@
 package capstone18_05.google.developers.httpsconsole.badgerbuddy;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class StartPage extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class StartPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+    }
+
+    public void onCreateClick(View view)
+    {
+        startActivity(new Intent(this, CreateAccount.class));
+    }
+
+    public void onLoginClick(View view)
+    {
+        startActivity(new Intent(this, AccountLogin.class));
     }
 
 
