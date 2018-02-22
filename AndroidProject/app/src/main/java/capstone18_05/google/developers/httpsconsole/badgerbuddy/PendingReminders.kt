42e840f94bc5167637c99e8d2771b98ce58984d8
colbyhuze93 <colbyhughes93@gmail.com>
2018-02-21 20:12:39 -0600

    Pending reminders UI

---
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/PendingReminders.kt
@@ -0,0 +1,12 @@
+package capstone18_05.google.developers.httpsconsole.badgerbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PendingReminders : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_reminders)
+    }
+}
