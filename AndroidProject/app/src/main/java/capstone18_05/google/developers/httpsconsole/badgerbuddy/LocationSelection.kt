ba325e5d167e827d3025d5208b7c6ec6826a477f
colbyhuze93 <colbyhughes93@gmail.com>
2018-02-21 19:35:54 -0600

    Location selection UI

---
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/LocationSelection.kt
@@ -0,0 +1,12 @@
+package capstone18_05.google.developers.httpsconsole.badgerbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationSelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_selection)
+    }
+}
