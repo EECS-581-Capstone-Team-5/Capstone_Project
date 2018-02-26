a0f5f94b97e7f233743f2214f2c12c7b7f77b05d
James Muoghalu <jmuoghalu00@outlook.com>
2018-02-25 21:25:32 -0600

    Beginning UI Navigation

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/AccountHomepage.java
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/AccountHomepage.java
@@ -1,19 +1,22 @@
 package capstone18_05.google.developers.httpsconsole.badgerbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class CreateReminders extends AppCompatActivity {
+public class AccountHomepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createrem);
+        setContentView(R.layout.activity_acchome);
     }
 
-    public void onButtonClick(View view)
+    public void onMakeNewRem(View view)
     {
-        finish();
+        startActivity(new Intent(this, CreateReminders.class));
     }
+
+
 }
