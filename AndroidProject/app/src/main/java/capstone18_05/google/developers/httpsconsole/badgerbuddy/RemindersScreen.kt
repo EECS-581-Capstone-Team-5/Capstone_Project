17518ef5d65150900e4a09b7f378b421975b335e
Alex Pechin <a800p737@ku.edu>
2018-02-26 16:19:47 -0600

    Reminders Screen added

---
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/RemindersScreen.kt
@@ -0,0 +1,12 @@
+package capstone18_05.google.developers.httpsconsole.badgerbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RemindersScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminders_screen)
+    }
+}
