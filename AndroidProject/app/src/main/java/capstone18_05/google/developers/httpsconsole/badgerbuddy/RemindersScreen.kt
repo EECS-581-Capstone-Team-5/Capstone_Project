f6232fe3443497fbe497a7b7b3ff816d0c47e03b
Alex Pechin <a800p737@ku.edu>
2018-02-26 17:49:47 -0600

    Reminders Screen navigation on buttons

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/RemindersScreen.kt
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/RemindersScreen.kt
@@ -1,12 +1,24 @@
 package capstone18_05.google.developers.httpsconsole.badgerbuddy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RemindersScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders_screen)
+    }
+
+    fun onAdd(view: View)
+    {
+        startActivity(Intent(this, CreateReminders::class.java))
+    }
+
+    fun onBuddy(view: View)
+    {
+        startActivity(Intent(this, Buddies_Screen::class.java))
     }
 }
