7e90831b11d90ec855c79c8eb04b097f482a0847
Richard Aviles <r556a812@ku.edu>
2018-02-26 18:07:34 -0600

    Update Buddies UI

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/Buddies_Screen.kt
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/Buddies_Screen.kt
@@ -1,12 +1,24 @@
 package capstone18_05.google.developers.httpsconsole.badgerbuddy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Buddies_Screen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buddies__screen)
+    }
+
+    fun onReminder(view: View)
+    {
+        startActivity(Intent(this, RemindersScreen::class.java))
+    }
+
+    fun onSearch(view: View)
+    {
+        startActivity(Intent(this, BuddySearch::class.java))
     }
 }
