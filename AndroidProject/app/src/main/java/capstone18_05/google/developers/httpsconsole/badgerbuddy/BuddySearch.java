fcc49a1b96e9b7f2eeab933fd8e340814f5c3f5b
Ashli Mosiman <amosiman93@gmail.com>
2018-03-04 17:13:31 -0600

    buddy search page works

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/BuddySearch.java
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/BuddySearch.java
@@ -2,7 +2,21 @@ package capstone18_05.google.developers.httpsconsole.badgerbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.SearchView;
 
-public class BuddySearch {
+public class BuddySearch extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buddysearch);
+        Intent intent = getIntent();
+    }
 }
