f4b2c849670c9a9d877eb8386078a61f76514ede
James Muoghalu <j286m692@ku.edu>
2018-03-20 19:00:24 -0500

    Minor Savepoint

--- AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/CreateReminders.java
+++ AndroidProject/app/src/main/java/capstone18_05/google/developers/httpsconsole/badgerbuddy/CreateReminders.java
@@ -3,6 +3,7 @@ package capstone18_05.google.developers.httpsconsole.badgerbuddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class CreateReminders extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class CreateReminders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createrem);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     public void onButtonClick(View view)
